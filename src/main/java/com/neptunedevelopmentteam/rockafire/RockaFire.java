eed1109d5455283ec27fcfa54488a7d062c2d0a3
Creativious <timothyu120@gmail.com>
2024-02-17 19:55:18 -0500

    First commit

---
+++ src/main/java/com/neptunedevelopmentteam/rockafire/RockaFire.java
@@ -0,0 +1,15 @@
+package com.neptunedevelopmentteam.rockafire;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class RockaFire implements ModInitializer {
+
+    public static final String MOD_ID = "rockafire";
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/neptunedevelopmentteam/rockafire/client/RockaFireClient.java
@@ -0,0 +1,10 @@
+package com.neptunedevelopmentteam.rockafire.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RockaFireClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
